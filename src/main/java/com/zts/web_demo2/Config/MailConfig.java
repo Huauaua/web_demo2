570a3b603d31bfd134e7d7cb463a01dbea9e15ba
Huauaua <1775778681@qq.com>
2026-02-07 00:46:01 +0800

    feat: 实现发送邮箱消息

--- src/main/java/com/zts/web_demo2/Config/MailConfig.java
+++ src/main/java/com/zts/web_demo2/Config/MailConfig.java
@@ -1,22 +1,17 @@
 package com.zts.web_demo2.Config;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+@Getter
 @Configuration
 public class MailConfig {
     
-    @Value("${contact.email.to:your-email@example.com}")
+    @Value("${contact.email.to:zhengtianhuo@qq.com}")
     private String toEmail;
     
-    @Value("${contact.email.from:website@yourdomain.com}")
+    @Value("${contact.email.from:zhengtianhuo@qq.com}")
     private String fromEmail;
-    
-    public String getToEmail() {
-        return toEmail;
-    }
-    
-    public String getFromEmail() {
-        return fromEmail;
-    }
+
 }
